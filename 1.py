0e75e19040f6eeb004971e8c1a1a0d78b6455c43
VladinasM <69033234+VladinasM@users.noreply.github.com>
2020-12-03 01:36:13 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,21 +1,8 @@
-f = open('input.txt')                                  
-print(f.read())
-result = []
-for line in f:
-   line = line.split()
-   result.append(line)
-print(" ".join(map(str, result)))
-
-
-
-
-
-
-f1 = 'input.txt'
-result = []
-with open(f1) as file:
-    print(file.read())
-    for line in f1:
-        line = line.split()
-        result.append(line)
-print(" ".join(map(str, result)))
+l = []
+with open('input.txt') as f:
+    l = f.read().splitlines()
+for i in range(len(l)-1, -1, -1):
+    if l[i] == ' ' :
+        if l[i - 1] == ' ':
+            l[i] = ''
+print(l)
